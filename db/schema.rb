484bb8cd2a013b3c5148700203a8b79d31b713c6
yuuuko86326274 <yuko.67.53@gmail.com>
2020-08-29 10:50:49 +0900

    ログイン機能、新規登録共に動作確認済

--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 2020_08_27_140747) do
     t.string "last_name"
     t.string "first_name_pkey"
     t.string "last_name_pkey"
-    t.string "birthday"
+    t.date "birthday"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
