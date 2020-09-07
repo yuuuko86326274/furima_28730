27e3b1e34a19f0659afaea67ac699776d8e9002f
yuuuko86326274 <yuko.67.53@gmail.com>
2020-09-07 18:35:33 +0900

    PR後修正①

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: %i[index show]
-  before_action :find_id, except: %i[index new create]
+  before_action :move_to_index, except: [:index, :show]
+  before_action :find_id, except: [:index, :new, :create]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -19,10 +19,6 @@ class ItemsController < ApplicationController
       render 'new'
     end
   end
-
-  def show; end
-
-  def edit; end
 
   def update
     if @item.update(item_params)
