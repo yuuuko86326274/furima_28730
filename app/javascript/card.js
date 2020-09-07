3c56a99e541f363dd3a22cc03c8b3c0df8d384a7
yuuuko86326274 <yuko.67.53@gmail.com>
2020-09-07 21:01:28 +0900

    test

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -14,7 +14,6 @@ const pay = () => {
       exp_year: `20${formData.get("exp_year")}`,
     };
     Payjp.createToken(card, (status, response) => {
-      console.log('OK')
       if (status == 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
